efdea9467ee9c3ee2deb71ddb0c7dfd2c8d1389b
Mikita Hlushkou <mikita.hlushkou@gmail.com>
2020-08-25 17:27:14 +0300

    refactor:move Promo.js to separate folder and change class names in it

---
+++ src/Pages/MainPage/Components/Promo/Promo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from '../Button/Button';
+import promoImage from '../../../../assets/images/promo.png';
+
+const Promo = ()=>{
+    return(
+       <div className="promo">
+           <img className={``} src={promoImage} alt=""/>
+           <div className='promo-container'>
+               <div className='promo-card'>
+                   <p className={`promo-description`}>Street fashion collection <br/> <span>Fall 2019</span> </p>
+                  <Button additionalClass={`promo`} text={`Shop now`} />
+               </div>
+           </div>
+       </div>
+    )
+}
+
+export default Promo;
