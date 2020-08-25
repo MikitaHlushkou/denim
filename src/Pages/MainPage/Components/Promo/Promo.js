7146022482c0b3adfa95efa2c3b8c92033b36a2f
Mikita Hlushkou <mikita.hlushkou@gmail.com>
2020-08-25 22:26:25 +0300

    fix: little fixes in styles

--- src/Pages/MainPage/Components/Promo/Promo.js
+++ src/Pages/MainPage/Components/Promo/Promo.js
@@ -5,7 +5,7 @@ import promoImage from '../../../../assets/images/promo.png';
 const Promo = ()=>{
     return(
        <div className="promo">
-           <img className={``} src={promoImage} alt=""/>
+           <img className={`promo-image`} src={promoImage} alt=""/>
            <div className='promo-container'>
                <div className='promo-card'>
                    <p className={`promo-description`}>Street fashion collection <br/> <span>Fall 2019</span> </p>
