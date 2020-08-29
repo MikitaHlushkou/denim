e3f84f2c69c3c6fe9bc529f1bf727c079944c453
Mikita Hlushkou <mikita.hlushkou@gmail.com>
2020-08-30 01:57:57 +0300

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 import MainPage from './Pages/MainPage';
 import Catalog from './Pages/Catalog';
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-<MainPage/>
-<Catalog/>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Route path='/main' component={MainPage}/>
+                <Route path='/catalog' component={Catalog}/>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
