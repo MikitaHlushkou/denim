5f3b43eba0e4136d4539ad12cd530d45c0809d7c
Mikita Hlushkou <mikita.hlushkou@gmail.com>
2020-08-29 21:21:47 +0300

    feat: add Filter with Dropdown list

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-let Filter = () => {
+let Filter = ({styles}) => {
     return (
-        <div>filtered By</div>
+        <div className={styles.filter}><form action="formdata" method="post" id="data"></form>
+            <p><select name="list1" form="data">
+                <option>Выберите из списка</option>
+                <option>Option</option>
+                <option>Textarea</option>
+                <option>Label</option>
+                <option>Fieldset</option>
+                <option>Legend</option>
+            </select></p>
+        </div>
+
     )
 }
 
