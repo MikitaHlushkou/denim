3f3bc47082f46506af93b3da4f24847511726eee
Mikita Hlushkou <mikita.hlushkou@gmail.com>
2020-09-01 02:18:31 +0300

    feat: add Filter html layout and function to toggle classlist

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -1,16 +1,56 @@
 import React from 'react';
 
-let Filter = ({styles}) => {
+let Filter = () => {
+    function myFunction(e) {
+        e.target.nextSibling.classList.toggle('show')
+
+    }
+
     return (
-        <div className={styles.filter}><form action="formdata" method="post" id="data"></form>
-            <p><select name="list1" form="data">
-                <option>Выберите из списка</option>
-                <option>Option</option>
-                <option>Textarea</option>
-                <option>Label</option>
-                <option>Fieldset</option>
-                <option>Legend</option>
-            </select></p>
+        <div className={`filter`}>
+
+            <button onClick={myFunction} className="dropbtn">Dropdown</button>
+            <div id="myDropdown" className="dropdown-content">
+                <div onClick={myFunction} className="dropbtn">Price</div>
+                <div id="drop" className="dropdown-content dropdown-content--secondary">
+                    <input type="range" min="1" max="100" defaultValue="50" className="slider" id="myRange"/>
+                </div>
+
+
+                <div onClick={myFunction} className="dropbtn">Color</div>
+                <div id="drop" className="dropdown-content dropdown-content--secondary">
+                    <div className="colorContainer">
+                        <div className="colorCircle colorCircle-lightGray "></div>
+                        <div className="colorCircle colorCircle-darkBlue"></div>
+                        <div className="colorCircle colorCircle-gray"></div>
+                        <div className="colorCircle colorCircle-black"></div>
+                        <div className="colorCircle colorCircle-white"></div>
+                        <div className="colorCircle colorCircle-blue"></div>
+                    </div>
+                </div>
+
+
+                <div onClick={myFunction} className="dropbtn">Collection</div>
+                <div id="drop" className="dropdown-content dropdown-content--secondary">
+                    <div className="collection">
+                        <div className="collection-name">Casual</div>
+                        <div className="collection-name">Petite</div>
+                        <div className="collection-name">Classic</div>
+                        <div className="collection-name">Soulluxe</div>
+                    </div>
+
+
+                </div>
+                <div className="filter-btnContainer">
+                    <button className="filter-btn">Apply</button>
+                </div>
+
+                <div className="filter-btnContainer">
+                    <button className="filter-btn">Clear all</button>
+                </div>
+            </div>
+
+
         </div>
 
     )
