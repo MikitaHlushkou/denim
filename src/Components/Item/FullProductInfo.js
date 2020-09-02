f9f469cf459437e7d5474d74293ae33c1d9fa382
Mikita Hlushkou <mikita.hlushkou@gmail.com>
2020-09-03 02:49:24 +0300

    refactor:change component to fulfill dom from props

--- src/Components/Item/FullProductInfo.js
+++ src/Components/Item/FullProductInfo.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Button from "../Button/Button";
 
-const FullProductInfo = () => {
+const FullProductInfo = ({state}) => {
+  let  {price, category,sizes} = state;
+       const size =sizes.map((el,i)=> <li key={i} className="size-item">{el}</li>)
     return (
         <div className="product-info">
-            <h2 className='product-title'>Overalls</h2>
-            <p className='product-description'>Overalls in washed organic cotton denim. Collar, button fly, and
+            <h2 className='product-title'>{state.category}</h2>
+            <p className='product-description'>{category} in washed organic cotton denim. Collar, button fly, and
                 adjustable belt at waist. Chest pockets
                 with flap. Long, gently flared sleeves and straight legs with front pockets and back pockets.</p>
             <div>
@@ -20,14 +22,10 @@ const FullProductInfo = () => {
             <div>
                 <h3 className='product-featureName'>Size</h3>
                 <ul className="size">
-                    <li className="size-item">XS</li>
-                    <li className="size-item">S</li>
-                    <li className="size-item">M</li>
-                    <li className="size-item">L</li>
-                    <li className="size-item">XL</li>
+                    {size}
                 </ul>
             </div>
-            <div>$160</div>
+            <div>$ {price}</div>
             <Button additionalClass={`addToCart`} text={`Add to cart`}/>
         </div>
     )
