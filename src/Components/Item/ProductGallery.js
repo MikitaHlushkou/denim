b811dc89a9022a8f7b076b69093053b9dc75c3c5
Mikita Hlushkou <mikita.hlushkou@gmail.com>
2020-09-03 00:48:09 +0300

    refactor:use hooks instead of setstate and so on

--- src/Components/Item/ProductGallery.js
+++ src/Components/Item/ProductGallery.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ProductGallery = ({state}) => {
-    let Product = state.picList.map((el, i) => <img className='product-image' src={el} key={state.picList[i]}
+    let Product = state.map((el, i) => <img className='product-image' src={el} key={state[i]}
                                                     alt={`product`}/>)
     return (
         <div className={`product-gallery`}>
--- src/Pages/ProductCard.js
+++ src/Pages/ProductCard.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {useRouteMatch} from 'react-router-dom';
 import HeaderCatalogPage from "../Components/Header/HeaderCatalogPage";
 import Category from "../Components/Category/Category";
 import item1 from "../assets/images/item1.png";
@@ -14,70 +15,55 @@ import Footer from "../Components/Footer/Footer";
 import ProductGallery from "../Components/Item/ProductGallery";
 import FullProductInfo from "../Components/Item/FullProductInfo";
 
-const itemArray = [{src: item1, type: 'Jacket', price: 120}, {src: item2, type: 'Overalls', price: 140}, {
+const itemArray = [{id:1,src: item1, type: 'Jacket', price: 120}, {id:2,src: item2, type: 'Overalls', price: 140}, {id:3,
     src: item3,
     type: 'Jacket',
     price: 90
-}, {src: item4, type: 'Overalls', price: 160}];
+}, {id:4,src: item4, type: 'Overalls', price: 160}];
 const pict = [{id: '1', images: [prod1, prod2, prod3, prod4, prod5]}, {
     id: '2',
     images: [prod5, prod4, prod3, prod2, prod1]
 }]
 
-class ProductCard extends React.Component {
-    state = {
-        index: 0,
-        picList: [],
-    }
-    Switch = (direction) => {
-        if (direction === 'right') {
+let ProductCard = () => {
+    const [picList, setPicList] = useState([]);
 
-            let arr = [...this.state.picList];
+    let Switch = (direction) => {
+        if (direction === 'right') {
+            let arr = [...picList];
             let prev = arr.shift();
             arr.push(prev);
-            this.setState({
-                picList: arr
-            })
+            setPicList(arr);
         } else if (direction === 'left') {
-            let arr = [...this.state.picList];
+            let arr = [...picList];
             let prev = arr.pop();
             arr.unshift(prev);
-            this.setState({
-                picList: arr
-            })
+            setPicList(arr);
         }
     }
+    const match = useRouteMatch('/product/:id?');
+    let id = match.params.id;
+    let filtered = pict.filter(el => el.id === id)[0];
 
-    id = this.props.match.params.id;
-    filtered = pict.filter(el => el.id === this.id)[0];
+    useEffect(() => id?setPicList(filtered.images):[],[])
 
-    componentWillMount() {
 
-        this.setState({
-            picList: this.filtered.images
-        })
-    }
+    return (
+        <div>
+            <HeaderCatalogPage/>
+            <main>
+                <section className="productCard">
+                    <div onClick={() => Switch('left')} className="arrow arrow-left"></div>
+                                      <ProductGallery id={id} state={picList}/>
+                    <div onClick={() => Switch('right')} className="arrow arrow-right"></div>
+                    <FullProductInfo/>
+                </section>
+                <Category items={itemArray} headline={`You may also like`}/>
+            </main>
+            <Footer additionalClass={`productPage`}/>
+        </div>
+    )
 
-    componentDidMount() {
-    }
-
-    render() {
-        return (
-            <div>
-                <HeaderCatalogPage/>
-                <main>
-                    <section className="productCard">
-                        <div onClick={() => this.Switch('left')} className="arrow arrow-left"></div>
-                        <ProductGallery id={this.id} filter={this.filtered.images} state={this.state}/>
-                        <div onClick={() => this.Switch('right')} className="arrow arrow-right"></div>
-                        <FullProductInfo/>
-                    </section>
-                    <Category items={itemArray} headline={`You may also like`}/>
-                </main>
-                <Footer additionalClass={`productPage`}/>
-            </div>
-        )
-    }
 }
 
 export default ProductCard;
