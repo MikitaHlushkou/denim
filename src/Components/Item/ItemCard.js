de14567d59f048a96a9542c09cb179b5190d5b34
Mikita Hlushkou <mikita.hlushkou@gmail.com>
2020-09-02 18:57:02 +0300

    refactor: change <a> to Link from router dom

--- src/Components/Item/ItemCard.js
+++ src/Components/Item/ItemCard.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
-let ItemCard =({src,type,price, styles})=>{
-    return(
-        <div className={ styles['item-card']}>
-        <a href="#">
-            <div>
-                <img className={`item-image`} src={src} alt=""/>
-            </div>
-        </a>
+let ItemCard = ({src, type, price, styles}) => {
+
+    return (
+
+        <div className={styles['item-card']}>
+            <Link to={'/product'}>
+                <div>
+                    <img className={`item-image`} src={src} alt=""/>
+                </div>
+            </Link>
+
 
             <div className={`item-label`}><p>{type}</p>
-                {price?<span>$ {price}</span>:null}
+                {price ? <span>$ {price}</span> : null}
 
             </div>
         </div>
