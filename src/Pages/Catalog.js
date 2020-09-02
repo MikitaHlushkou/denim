def0406a4453c2dd8a45b80626385a4d7854cd5b
Mikita Hlushkou <mikita.hlushkou@gmail.com>
2020-09-02 17:18:19 +0300

    feat: add  additionalClass coming from props to better styling in different pages

--- src/Pages/Catalog.js
+++ src/Pages/Catalog.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import Footer from '../Components/Footer/Footer';
-import item1 from '../assets/images/item1.png';
-import item2 from '../assets/images/item2.png';
-import item3 from '../assets/images/item3.png';
-import item4 from '../assets/images/item4.png';
 import catalog1 from '../assets/images/1catalog.png';
 import catalog2 from '../assets/images/2catalog.png';
 import catalog3 from '../assets/images/3catalog.png';
@@ -13,10 +9,6 @@ import catalog6 from '../assets/images/6catalog.png';
 import catalog7 from '../assets/images/7catalog.png';
 import catalog8 from '../assets/images/8catalog.png';
 import catalog9 from '../assets/images/9catalog.png';
-import jeansCategory from '../assets/images/Jeans.png';
-import jacketsCategory from '../assets/images/Jackets-category.png';
-import shirtsCategory from '../assets/images/Shirts-category.png';
-import overallsCategory from '../assets/images/Overalls-category.png';
 import HeaderCatalogPage from '../Components/Header/HeaderCatalogPage';
 import CatalogDescription from '../Components/Header/CatalogDescription';
 import Filter from '../Components/Filter/Filter';
@@ -24,15 +16,6 @@ import ItemsCollection from '../Components/Item/ItemsCollection';
 import NumberOfItems from '../Components/Item/NumberOfItems';
 import CatalogStyles from '../Components/Item/Catalog.module.scss'
 
-const categoryArray = [{src: jeansCategory, type: 'Jeans'}, {
-    src: shirtsCategory,
-    type: 'Shirts'
-}, {src: jacketsCategory, type: 'Jackets'}, {src: overallsCategory, type: 'Overalls'}]
-const itemArray = [{src: item1, type: 'Jacket', price: 120}, {src: item2, type: 'Overalls', price: 140}, {
-    src: item3,
-    type: 'Jacket',
-    price: 90
-}, {src: item4, type: 'Overalls', price: 160}];
 const catalogArray = [{src: catalog1, type: 'Jacket', price: 40}, {
     src: catalog2,
     type: 'Jacket',
@@ -57,7 +40,7 @@ let Catalog = () => {
                 <ItemsCollection styles={CatalogStyles} items={catalogArray}/>
                 <NumberOfItems styles={CatalogStyles}/>
             </main>
-            <Footer/>
+            <Footer additionalClass={`catalogPage`}/>
         </div>
 
     )
--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -11,8 +11,16 @@ import jeansCategory from '../assets/images/Jeans.png';
 import jacketsCategory from '../assets/images/Jackets-category.png';
 import shirtsCategory from '../assets/images/Shirts-category.png';
 import overallsCategory from '../assets/images/Overalls-category.png';
-const categoryArray = [{src:jeansCategory,type:'Jeans'},{src:shirtsCategory,type:'Shirts'},{src:jacketsCategory,type:'Jackets'},{src:overallsCategory,type:'Overalls'}]
-const itemArray = [{src:item1,type:'Jacket',price:120}, {src:item2,type:'Overalls',price:140}, {src:item3,type:'Jacket',price:90}, {src:item4,type:'Overalls',price:160}];
+
+const categoryArray = [{src: jeansCategory, type: 'Jeans'}, {
+    src: shirtsCategory,
+    type: 'Shirts'
+}, {src: jacketsCategory, type: 'Jackets'}, {src: overallsCategory, type: 'Overalls'}]
+const itemArray = [{src: item1, type: 'Jacket', price: 120}, {src: item2, type: 'Overalls', price: 140}, {
+    src: item3,
+    type: 'Jacket',
+    price: 90
+}, {src: item4, type: 'Overalls', price: 160}];
 let MainPage = () => {
     return (
         <div>
@@ -22,7 +30,7 @@ let MainPage = () => {
                 <Promo/>
                 <Category items={categoryArray} headline={`Shop by category`}/>
             </main>
-            <Footer/>
+            <Footer additionalClass={'mainPage'}/>
         </div>
 
     )
